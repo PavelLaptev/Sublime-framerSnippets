750d83068f9d29ad76dabafc096b633ca50b894a
Pavel <graphics.dario@gmail.com>
2016-04-18 19:37:08 +0500

    add snippets, text module

--- Project/app.coffee
+++ Project/app.coffee
@@ -1,27 +1,19 @@
-# cleaning console - ClearConsole
+## cleaning console || trigger - ClearConsole ##
 console.clear()
 
-# new layer Iphone6 gray || target - newIphoneGray
+## init text layers || trigger - initText ##
+{TextLayer} = require 'TextLayer'
+
+
+## new layer Iphone6 gray || trigger - newIphone ##
 F = Framer
-#create background
+# create background
 frame = new Layer
-    image: 'images/iphone6-gray.jpg'
+    image: 'images/iphone6-white.jpg'
     height:2060
     width:1290
 frame.center()
-F.Defaults.Layer.parent = frame
-
-# screen sizes for devices
-if Utils.isDesktop()
-	frame.scale = (Screen.height / frame.height) / 1.5
-	window.onresize = ->
-	 	frame.scale = (Screen.height / frame.height) / 1.5
-	 	frame.center()
-if Utils.isMobile()
-	screen.scale = Screen.height / screen.height
-	window.onresize = ->
-	 	screen.scale = Screen.height / screen.height
-
+#
 screen = new Layer
 	backgroundColor: '#263238'
 	width:750
@@ -30,35 +22,98 @@ screen = new Layer
 	clip: true
 screen.center()
 F.Defaults.Layer.parent = screen
+# screen sizes for devices
+if Utils.isDesktop()
+	screen.scale = (Screen.height / screen.height) / 2
+	frame.scale = (Screen.height / screen.height) / 2
+	window.onresize = ->
+	 	screen.scale = (Screen.height / screen.height) / 2
+	 	frame.scale = (Screen.height / screen.height) / 2
+	 	screen.center()
+	 	frame.center()
+if Utils.isMobile()
+	frame.visible = false
+	screen.scale = Screen.height / screen.outerHeight
+	window.onresize = ->
+	 	screen.scale = Screen.height / screen.outerHeight
 
-#new layer
+
+## new layer || trigger - newLayer ##
 newLayer = new Layer
 	backgroundColor: Color.random()
+	opacity:0.5
 	width: 96
 	height: 96
 newLayer.center()
 
-#tabBar
-tabBar = new Layer
+
+## new Circle || trigger - newCircle ##
+newCircle = new Layer
+	backgroundColor: Color.random()
+	opacity:0.5
+	width: 96
+	height: 96
+	borderRadius: 10000
+newCircle.center()
+
+
+## new text layer || trigger - newText ## || init textLayer by trigger initText before use snippet
+newText = new TextLayer
+    text: "The quick text"
+    color: "#aaa"
+    textAlign: "center"
+    fontSize: 48
+    width: 200
+    autoSize: true
+    fontFamily: "Arial"
+newText.center()
+
+
+## tabbar || trigger - newTabbar ##
+tabbar = new Layer
 	backgroundColor: Color.random()
 	width: screen.width
 	height: 96
 	x: Align.left
 	y: Align.bottom
 
-#navBar
-statusBar = new Layer
-	image: 'images/iphone6-status.png'
-	backgroundColor: 'rgba(255,255,255,0.2)'
+
+## navigationbar with statusbar || trigger - newNavbar ##
+navbarContainer = new Layer
+	backgroundColor: Color.random()
+	height: 128
 	width: screen.width
+	x: Align.left
+	y: Align.top
+# statusBar
+statusbar = new Layer
+	image: 'images/iphone6-status.png'
+	width: navbarContainer.width
 	height: 40
 	x: Align.left
 	y: Align.top
-statusBar.placeBefore(navBar)
-
-navBar = new Layer
-	backgroundColor: 'rgba(255,255,255,0.2)'
-	width: screen.width
-	height: 128
+statusbar.parent = navbarContainer
+if Utils.isMobile()
+	statusbar.visible = false
+# navBar
+navbar = new Layer
+	backgroundColor: 'rgba(0,0,0,0.2)'
+	width: navbarContainer.width
+	height: 88
 	x: Align.left
-	y: Align.top
+	y: 40
+navbar.parent = navbarContainer
+
+
+#### EVENTS ####
+
+# drag || trigger - drag
+newLayer.draggable.enabled = true
+newLayer.draggable.vertical = false
+newLayer.draggable.constraints = newLayer
+
+# add states || trigger - state
+newLayer.states.add
+	StateA: x: 100, y: 100
+newLayer.states.animationOptions = curve: "spring(300, 0, 20)", time: .2
+
--- Project/app.js
+++ Project/app.js
@@ -1,34 +1,19 @@
 (function() {
-  var F, frame, navBar, newLayer, screen, statusBar, tabBar;
+  var F, TextLayer, frame, navbar, navbarContainer, newCircle, newLayer, newText, screen, statusbar, tabbar;
 
   console.clear();
+
+  TextLayer = require('TextLayer').TextLayer;
 
   F = Framer;
 
   frame = new Layer({
-    image: 'images/iphone6-gray.jpg',
+    image: 'images/iphone6-white.jpg',
     height: 2060,
     width: 1290
   });
 
   frame.center();
-
-  F.Defaults.Layer.parent = frame;
-
-  if (Utils.isDesktop()) {
-    frame.scale = (Screen.height / frame.height) / 1.5;
-    window.onresize = function() {
-      frame.scale = (Screen.height / frame.height) / 1.5;
-      return frame.center();
-    };
-  }
-
-  if (Utils.isMobile()) {
-    screen.scale = Screen.height / screen.height;
-    window.onresize = function() {
-      return screen.scale = Screen.height / screen.height;
-    };
-  }
 
   screen = new Layer({
     backgroundColor: '#263238',
@@ -42,15 +27,57 @@
 
   F.Defaults.Layer.parent = screen;
 
+  if (Utils.isDesktop()) {
+    screen.scale = (Screen.height / screen.height) / 2;
+    frame.scale = (Screen.height / screen.height) / 2;
+    window.onresize = function() {
+      screen.scale = (Screen.height / screen.height) / 2;
+      frame.scale = (Screen.height / screen.height) / 2;
+      screen.center();
+      return frame.center();
+    };
+  }
+
+  if (Utils.isMobile()) {
+    frame.visible = false;
+    screen.scale = Screen.height / screen.outerHeight;
+    window.onresize = function() {
+      return screen.scale = Screen.height / screen.outerHeight;
+    };
+  }
+
   newLayer = new Layer({
     backgroundColor: Color.random(),
+    opacity: 0.5,
     width: 96,
     height: 96
   });
 
   newLayer.center();
 
-  tabBar = new Layer({
+  newCircle = new Layer({
+    backgroundColor: Color.random(),
+    opacity: 0.5,
+    width: 96,
+    height: 96,
+    borderRadius: 10000
+  });
+
+  newCircle.center();
+
+  newText = new TextLayer({
+    text: "The quick text",
+    color: "#aaa",
+    textAlign: "center",
+    fontSize: 48,
+    width: 200,
+    autoSize: true,
+    fontFamily: "Arial"
+  });
+
+  newText.center();
+
+  tabbar = new Layer({
     backgroundColor: Color.random(),
     width: screen.width,
     height: 96,
@@ -58,23 +85,54 @@
     y: Align.bottom
   });
 
-  statusBar = new Layer({
-    image: 'images/iphone6-status.png',
-    backgroundColor: 'rgba(255,255,255,0.2)',
+  navbarContainer = new Layer({
+    backgroundColor: Color.random(),
+    height: 128,
     width: screen.width,
+    x: Align.left,
+    y: Align.top
+  });
+
+  statusbar = new Layer({
+    image: 'images/iphone6-status.png',
+    width: navbarContainer.width,
     height: 40,
     x: Align.left,
     y: Align.top
   });
 
-  statusBar.placeBefore(navBar);
+  statusbar.parent = navbarContainer;
 
-  navBar = new Layer({
-    backgroundColor: 'rgba(255,255,255,0.2)',
-    width: screen.width,
-    height: 128,
+  if (Utils.isMobile()) {
+    statusbar.visible = false;
+  }
+
+  navbar = new Layer({
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    width: navbarContainer.width,
+    height: 88,
     x: Align.left,
-    y: Align.top
+    y: 40
   });
+
+  navbar.parent = navbarContainer;
+
+  newLayer.draggable.enabled = true;
+
+  newLayer.draggable.vertical = false;
+
+  newLayer.draggable.constraints = newLayer;
+
+  newLayer.states.add({
+    StateA: {
+      x: 100,
+      y: 100
+    }
+  });
+
+  newLayer.states.animationOptions = {
+    curve: "spring(300, 0, 20)",
+    time: .2
+  };
 
 }).call(this);
